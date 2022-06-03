b4b27527274b03bc09780ade8398c015b205f606
ayeshaparvin <shakhawat.hosen@northsouth.edu>
2022-06-03 11:38:16 +0600

    added comment

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function displayPost(){
+/* function displayPost(){
     fetch('https://jsonplaceholder.typicode.com/posts')
 .then(response => response.json())
 .then(data => createPost(data))
@@ -18,5 +18,26 @@ function createPost(posts){
        div.classList.add('post');
        
        postId.appendChild(div);
+    }
+} */
+
+function displayComment(){
+    fetch('https://jsonplaceholder.typicode.com/comments')
+.then(response => response.json())
+.then(data => findComment(data));
+}
+
+displayComment();
+
+
+function findComment(comments){
+    const commentId = document.getElementById('comment-id');
+    for(const comment of comments){
+        const div = document.createElement('div');
+        div.innerHTML = `<h1>name:${comment.name}</h1>
+        <h3>Email:${comment.email}</h3>
+        <p>Details:${comment.body}</p>
+        `;
+        commentId.appendChild(div);
     }
 }
